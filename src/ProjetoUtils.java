fb0f56168ef6fd9e313350c684d5c0d92c2853fe
plynyo <pjg.oliveira21@gmail.com>
2022-10-30 00:56:26 +0100

    Código Finalizado (em principio)

--- src/ProjetoUtils.java
+++ src/ProjetoUtils.java
@@ -1,30 +1,41 @@
 import java.util.Random;
 public class ProjetoUtils {
+    //Códigos para permitir cores
     static String AZUL="\u001B[34m";
     static String RESET="\u001B[0m";
+
+    //Variável para permitir geração de numeros aleatórios a partir da biblioteca java.util.Random
     static Random randomgen = new Random();
+
+    //Função utilizada para processar TODOS os inputs do utilizador
     public static int processarInput(String input) {
+        //Remover espaços e outras interferências à integridade da String
+        input=input.trim();
+
         int input_processado;
 
+        //Se o utilizador der input de um espaço em branco, retorna -2,
+        //um código pré-definido já identificado pelo corpo principal do programa
         if(input.equals("")) {
             return -2;
-        } else {
-            //Detetar inputs inválidos
+        }
+
+        else {
             try {
-                //Se o input do utilizador não for convertível em inteiro
-                //Significa que inseriu um valor não numérico ou numérico com decimais
-                //Será lançado um erro na linha a seguir
-                //Que será tratado retornando -1 à chamada da função
-                //Caso o input seja válido, será retornado na forma de inteiro, de forma a ser utilizado
+                //Se o input do utilizador não for convertível em inteiro,
+                //significa que inseriu um valor não numérico ou numérico com decimais,
+                //e será lançado um erro na linha a seguir
                 input_processado = Integer.parseInt(input);
             } catch (Exception e) {
+                //Este erro será tratado retornando -1 à chamada da função
+                //um código pré-definido já identificado pelo corpo principal do programa
                 return -1;
             }
-
+            //Caso o input seja válido, será retornado na forma de inteiro, de forma a ser utilizado
             return input_processado;
         }
 
-        //outputs desta função
+        //Outputs desta função
         //Input = espaço em branco(Enter) -> Output = -2
         //Input = string não numérica inteira -> Output = -1
         //Input = string numérica inteira -> Output = (int)input
@@ -32,49 +43,61 @@ public class ProjetoUtils {
     public static int[][] gerarArray() {
         //Criar array 2d
         int[][] matriz = new int[9][9];
-        int n;
+        int tempNum;
+        //Percorrer todos os espaços do array 2d
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y < 9; y++) {
                 do {
-                    //Gerar numero aleatório de 0 a 9
-                    n = randomgen.nextInt(10);
-                } while (checkDup(matriz,n, x, y));
-                //A variavel "n" vai receber numeros aleatórios até passar na função checkDup
-                //Esta função verifica se existem duplicados
+                    //Gerar numero aleatório de 0 a 9 (10 numeros)
+                    tempNum = randomgen.nextInt(10);
+                } while (verificarDuplicados(matriz,tempNum, x, y));
+                //A variavel "tempNum" vai receber numeros aleatórios até passar na função verificarDuplicados
+                //Esta função verifica se existem duplicados na linha e coluna atual da geração
 
-                //Quando "n" receber um numero aleatório que não se repete ou 0,
+                //Quando "n" receber um numero aleatório que não se repete ou que seja 0,
                 //esse valor é aplicado na tabela final
-                matriz[x][y]=n;
+                matriz[x][y]=tempNum;
             }
         }
+        //Retornar a tabela preenchida
         return matriz;
     }
-    public static boolean checkDup(int[][] matriz,int n, int x, int y) {
-        //Se "n" for 0, é retornada a informação de não é duplicado, porque 0 não passa por essa verificação
-        if(n==0) return false;
+
+    //Função para verificar duplicados com o numero recebido numa certa linha e coluna
+    public static boolean verificarDuplicados(int[][] matriz, int tempNum, int x, int y) {
+        //Se "tempNum" for 0, é retornado não duplicado, porque 0 não passa por essa verificação
+        if(tempNum==0) return false;
+
         for (int z = 0; z < 9; z++) {
             //Verificar linhas
             if (z != y) {
-                if (n == matriz[x][z]) return true;
+                if (tempNum == matriz[x][z]) return true;
             }
             //Verificar colunas
             if (z != x) {
-                if (n == matriz[z][y]) return true;
+                if (tempNum == matriz[z][y]) return true;
             }
         }
         return false;
     }
+
+    //Função para obter os numeros que podem entrar numa determinada posição sem estarem duplicados na linha e coluna
     public static int[] obterPossiveis(int[][] matriz,int x, int y) {
         boolean arrayVazio=true;
         int[] possiveis = new int[9];
+
         for (int i = 1; i <= 9; i++) {
-            if(!checkDup(matriz,i,x,y)) {
+            if(!verificarDuplicados(matriz,i,x,y)) {
                 possiveis[i - 1] = i;
                 arrayVazio=false;
             }
         }
+        //Se não existirem numeros possiveis retorna um array nulo,
+        //caso contrário retorna um array com os numeros possiveis
         return (arrayVazio ? new int[0] : possiveis);
     }
+
+    //Método para mostrar o array 2d recebido
     public static void mostrarArray(int[][] array) {
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y < 9; y++) {
@@ -83,10 +106,12 @@ public class ProjetoUtils {
             System.out.println();
         }
     }
-    public static void mostrarArray(int[][] array, int Nx, int Ny) {
+
+    //Método overloaded para mostrar o array 2d recebido e destacar um determinado numero com cor
+    public static void mostrarArray(int[][] array, int NumX, int NumY) {
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y < 9; y++) {
-                if(x==Nx && y==Ny) System.out.print(AZUL+array[x][y]+" "+RESET);
+                if(x==NumX && y==NumY) System.out.print(AZUL+array[x][y]+" "+RESET);
                 else System.out.print(array[x][y]+" ");
             }
             System.out.println();
--- src/projeto.java
+++ src/projeto.java
@@ -1,9 +1,15 @@
 import java.util.Random;
 import java.util.Scanner;
 public class projeto {
+    //Códigos para permitir cores
+    static String AZUL="\u001B[34m";
     static String RESET="\u001B[0m";
     static String VERMELHO = "\u001B[31m";
+
+    //Variável scanner para entrada do utilizador pelo terminal
     static Scanner userInput = new Scanner(System.in);
+
+    //Variável para permitir geração de numeros aleatórios a partir da biblioteca java.util.Random
     static Random randomgen = new Random();
     public static void main(String[] args) {
         System.out.println("\n ---------------------------------------------------");
@@ -12,11 +18,14 @@ public class projeto {
         System.out.println(" ---------------------------------------------------");
         boolean sair=false;
         do {
+            //O switch receberá o valor retornado pelo menu (escolha do utilizador já validada)
             switch (menu()) {
                 case 1 -> padroes();
                 case 2 -> adivinha();
                 case 3 -> sudoku();
                 case 4 -> {
+                    //O utilizador escolheu sair do programa
+                    //é apresentada a mensagem de saída e acionada a flag de fim de programa
                     System.out.println("Obrigado por ter utilizado o programa, preesione ENTER para sair.");
                     userInput.nextLine();
                     sair=true;
@@ -27,19 +36,20 @@ public class projeto {
     public static int menu() {
         int escolhaMenu;
 
-        System.out.println("\n --------/ Menu Principal /--------- \n");
-        System.out.println("(1) Desenho de padrões");
-        System.out.println("(2) Jogo do adivinha");
-        System.out.println("(3) Sudoku simplificado");
-        System.out.println("(4) Sair do programa\n");
-
+        System.out.println("\n --------/ "+AZUL+"Menu Principal"+RESET+" /--------\n");
+        System.out.println(AZUL+"(1)"+RESET+" Desenho de padrões");
+        System.out.println(AZUL+"(2)"+RESET+" Jogo do adivinha");
+        System.out.println(AZUL+"(3)"+RESET+" Sudoku simplificado");
+        System.out.println(AZUL+"(4)"+RESET+" Sair do programa\n");
 
         do {
             System.out.print("Escolha: ");
+            //A escolha do utilizador é guardada na respetiva variável após passar pela função para validar o input
             escolhaMenu=ProjetoUtils.processarInput(userInput.nextLine());
 
             //Se o utilizador escolher uma opção fora dos limites do menu
             if(escolhaMenu<1 || escolhaMenu>4) {
+                //No caso do utilizador inserir um espaço em branco, apenas ignora e prossegue a pedir um input novo
                 if(escolhaMenu==-2) continue;
 
                 System.out.println(VERMELHO+"Escolha inválida"+RESET);
@@ -57,16 +67,18 @@ public class projeto {
     }
     public static void padroes() {
         int numeroInserido,numeroMostrar;
+        System.out.println("\n --------/ "+AZUL+"Desenho de Padrões"+RESET+" /--------\n");
         do {
-            System.out.println(" ---------/ Desenho de Padrões /--------- \n");
             System.out.print("Insira um numero entre 1 e 10: ");
 
+            //O numero inserido pelo utilizador é guardada na respetiva variável após passar pela função para validar o input
             numeroInserido=ProjetoUtils.processarInput(userInput.nextLine());
 
             if(numeroInserido<1 || numeroInserido>10) {
+                //No caso do utilizador inserir um espaço em branco, apenas ignora e prossegue a pedir um input novo
                 if(numeroInserido==-2) continue;
                 System.out.println(VERMELHO+"""
-                        Escolha inválida
+                        Número inválido
                         Lembre-se do intervalo de números possíveis (1-10) e também de inserir apenas números inteiros.
                         """+RESET);
                 continue;
@@ -76,7 +88,7 @@ public class projeto {
         }while(true);
 
         //Padrão A
-        System.out.println(" --/ Padrão A /-- ");
+        System.out.println("\n ----/ "+AZUL+"Padrão A"+RESET+" /----");
         for (int i = 0; i < numeroInserido; i++) {
             numeroMostrar = 1;
             for (int j = 0; j <=i; j++) {
@@ -89,7 +101,7 @@ public class projeto {
         System.out.println();
 
         //Padrão B
-        System.out.println(" --/ Padrão B /-- ");
+        System.out.println("\n ----/ "+AZUL+"Padrão B"+RESET+" /----");
         for (int i = numeroInserido; i >=1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j+" ");
@@ -100,7 +112,7 @@ public class projeto {
         System.out.println();
 
         //Padrão C
-        System.out.println(" --/ Padrão C /-- ");
+        System.out.println("\n ----/ "+AZUL+"Padrão C"+RESET+" /----");
         for (int i = 0; i < numeroInserido; i++) {
             numeroMostrar = i+1;
             for (int x=numeroInserido-i; x>1; x--) { //Imprime espaços por linha antes dos numeros
@@ -119,13 +131,17 @@ public class projeto {
         //Gerar numero de 0 a 1000 (1001 numeros)
         int numeroGerado=randomgen.nextInt(1001);
 
-        System.out.println("\n ---------/ Jogo Da Adivinha /--------- \n");
+        System.out.println("\n --------/ "+AZUL+"Jogo da Adivinha"+RESET+" /--------\n");
         System.out.println("O programa gerou um numero aleatório entre 0 e 1000.");
         System.out.println("Tente adivinhá-lo!");
 
         do {
-            System.out.print("A sua tentativa -> ");
+            System.out.print(AZUL+"-> "+RESET);
+            //O numero inserido pelo utilizador é guardada na respetiva variável após passar pela função para validar o input
             tentativa=ProjetoUtils.processarInput(userInput.nextLine());
+
+            //Inserido espaço em branco
+            if(tentativa==-2) continue;
 
             if(tentativa<0 || tentativa>1000) {
                 System.out.print(VERMELHO+"""
@@ -146,79 +162,89 @@ public class projeto {
         } while(tentativa != numeroGerado);
     }
     public static void sudoku() {
+        System.out.println("\n ---------/ "+AZUL+"Sudoku Simplificado"+RESET+" /--------- \n");
+
+        //Gerar array e guardá-lo na respetiva variável
         int[][] matriz = ProjetoUtils.gerarArray();
+        //Mostrar tabela
         ProjetoUtils.mostrarArray(matriz);
-        int x,y;
 
+        System.out.println("\nEscolha agora uma posição que contenha o numero 0, e siga os passos para o preencher");
         while(true) {
-            try {
-                System.out.print("Insira a coluna e linha no seguinte formato - lin,col: ");
-                String cordenadas=userInput.nextLine();
 
-                String Sx = cordenadas.split(",")[0];
-                String Sy = cordenadas.split(",")[1];
+            System.out.print("Insira a coluna: ");
+            int y=ProjetoUtils.processarInput(userInput.nextLine())-1;
 
-                x = Integer.parseInt(Sx) - 1;
-                y = Integer.parseInt(Sy) - 1;
+            System.out.print("\nInsira a linha: ");
+            int x=ProjetoUtils.processarInput(userInput.nextLine())-1;
 
-                if (x < 0 || x > 8 || y < 0 || y > 8) {
-                    System.out.println(VERMELHO+"Posição inválida\n" +
-                            "Lembre-se do intervalo de posições possíveis \"(1-9),(1-9)\" e também de inserir apenas números inteiros."+RESET);
-                    continue;
-
-                } else if (matriz[x][y] != 0) {
-                    System.out.println(VERMELHO+"A posição que inseriu não contém um 0 (" + matriz[x][y] + ")"+RESET);
-                    continue;
-                }
-                break;
-            } catch (Exception e) {
-                System.out.println(VERMELHO+"Não foi possível obter a posição, tente novamente."+RESET);
+            if (x < 0 || x > 8 || y < 0 || y > 8) {
+                System.out.println(VERMELHO+"Posição inválida\n" +
+                        "Lembre-se do intervalo de posições possíveis \"(1-9)\" e também de inserir apenas números inteiros."+RESET);
+                continue;
             }
-        }
 
-        int[] possibilidades = ProjetoUtils.obterPossiveis(matriz,x,y);
+            else if (matriz[x][y] != 0) {
+                System.out.println(VERMELHO+"A posição que inseriu não contém um 0 (contém " + matriz[x][y] + ")"+RESET);
+                continue;
+            }
 
-        if(possibilidades.length==0) System.out.println("A posição referida não contém numeros possiveis.");
-        else {
-            System.out.println("Numeros possiveis: ");
-            for (int i = 0; i < 9; i++) if(possibilidades[i]!=0) System.out.print(possibilidades[i]+" ");
+            else {
+                //Obter numeros possiveis da posição pretendida
+                int[] possibilidades = ProjetoUtils.obterPossiveis(matriz, x, y);
 
-            int numEscolhido;
-            while(true) {
-                System.out.print("Insira o número possivel que deseja escolher: ");
-
-                numEscolhido = ProjetoUtils.processarInput(userInput.nextLine());
-                if(numEscolhido==0) {
-                    System.out.println("Não pode inserir zeros.");
+                //Se a função retornar um vetor nulo (pré-programada para se comportar como tal se não existirem numeros possiveis)
+                if (possibilidades.length == 0) {
+                    System.out.println("A posição referida não contém numeros possiveis.");
                     continue;
                 }
 
-                //Espaço em branco
-                if(numEscolhido==-2) continue;
-
-                //String não numérica
-                if(numEscolhido==-1) System.out.println(VERMELHO+"Insira um número válido!"+RESET);
-
+                //Se existirem numeros possiveis
                 else {
-                    //Verificar se o numero escolhido existe dentro do array
-                    boolean existe=false;
+                    System.out.print("Numeros possiveis: ");
+
+                    //Listar numeros possiveis
                     for (int i = 0; i < 9; i++) {
-                        if(numEscolhido==possibilidades[i]) {
-                            existe = true;
-                            break;
+                        if(possibilidades[i]!=0) System.out.print(possibilidades[i]+" ");
+                    }
+
+                    while(true) {
+                        System.out.print("\nInsira o número possivel que deseja escolher: ");
+
+                        //O numero inserido pelo utilizador é guardada na respetiva variável após passar pela função para validar o input
+                        int numEscolhido = ProjetoUtils.processarInput(userInput.nextLine());
+
+                        //Espaço em branco
+                        if (numEscolhido == -2) continue;
+
+                        //String não numérica
+                        if (numEscolhido == -1) System.out.println(VERMELHO + "Insira um número válido!" + RESET);
+
+                        else {
+                            //Flag
+                            boolean existe = false;
+                            //Verificar se o numero escolhido existe dentro do array
+                            for (int i = 0; i < 9; i++) {
+                                if (numEscolhido == possibilidades[i] && numEscolhido != 0) {
+                                    existe = true;
+                                    break;
+                                }
+                            }
+                            if (existe) {
+                                //Finalmente aplicar o numero à tabela final
+                                matriz[x][y] = numEscolhido;
+                                System.out.println("Aqui está apresentada a tabela final com o número modificado sendo destacado a azul:\n");
+                                //Exibir a tabela final
+                                ProjetoUtils.mostrarArray(matriz, x, y);
+                                break;
+                            } else {
+                                System.out.println(VERMELHO + "O número que inseriu não é um numero possivel." + RESET);
+                            }
                         }
                     }
-                    if(existe) {
-                        matriz[x][y]=numEscolhido;
-                        ProjetoUtils.mostrarArray(matriz,x,y);
-                    }
-                    else {
-                        System.out.println(VERMELHO+"O número que inseriu não é um numero possivel."+RESET);
-                        continue;
-                    }
-                    break;
                 }
             }
+            break;
         }
     }
 }
